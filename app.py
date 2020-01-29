8e4c6ae645ba4af1936a79c0a764b64b654f117c
Saher Ahmad <saherahmad96@gmail.com>
2020-01-29 18:56:14 -0500

    new

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 from flask import Flask, render_template, jsonify
-from flask_pymongo import PyMongo
-import jinja2
+import pymongo
 
 Portfolio = Flask(__name__)
 
 @Portfolio.route('/Homepage')
-def Homepage();
+def Homepage():
     return render_template('home.html', name='Saher')
 
 @Portfolio.route('/AboutMe')
@@ -13,13 +12,13 @@ def AboutMe():
     return render_template('aboutme.html', name='Saher')
 
 @Portfolio.route('/ContactForm')
-def AboutMe():
+def ContactForm():
     return render_template('contactform.html', name='Saher')
 
-@Portfolio.route('/Portfolio')
-def AboutMe():
+@Portfolio.route('/MyPortfolio')
+def ShowPortfolio():
     return render_template('portfolio.html', name='Saher')
 
-if__name__ == '__main__':
+if __name__ == '__main__':
     Portfolio.run()
 
